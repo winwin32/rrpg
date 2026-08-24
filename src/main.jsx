24a816b604c1dd4ba71869692b116776fdb6cde5
winwin32 <collinbrun@gmail.com>
2026-08-24 06:10:40 -0500

    hosting fix

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(
     document.getElementById("root")
 ).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/rrpg">
             <App />
         </BrowserRouter>
     </React.StrictMode>
